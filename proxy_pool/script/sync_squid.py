779367ac2d1d5dbbab077a2866893e6634011ff3
king.lei <kinglei@kingleideMBP.lan>
2018-09-18 22:04:40 +0800

    修改squid的最大连接数

--- proxy_pool/script/sync_squid.py
+++ proxy_pool/script/sync_squid.py
@@ -15,7 +15,7 @@ def update_squid_conf(default_conf_path, conf_path):
         return False
     proxy_conf = ''
     for _info in proxy_list:
-        proxy_conf += 'cache_peer %s parent %d 0 no-query weighted-round-robin weight=%d connect-fail-limit=2 allow-miss max_conn=5 name=proxy-%d\n' % (
+        proxy_conf += 'cache_peer %s parent %d 0 no-query weighted-round-robin weight=%d connect-fail-limit=2 allow-miss max_conn=2 name=proxy-%d\n' % (
             _info.ip, _info.port, _info.score, _info.id)
 
     with open(default_conf_path, 'r') as f:
